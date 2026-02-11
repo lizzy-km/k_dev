43270d2e32b4070e85f6b348118c557dea78c558
Fuck the Error! <kaungmyatsoe.devk@gmail.com>
2026-02-11 22:06:14 +0630

    Add arrow icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,12 @@ import { Projects } from '@/components/sections/Projects';
 import { FigmaTool } from '@/components/sections/FigmaTool';
 import { Contact } from '@/components/sections/Contact';
 import { Toaster } from '@/components/ui/toaster';
+import Link from 'next/link';
+import { ArrowBigDown, ArrowDown } from 'lucide-react';
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
+    <main className="min-h-screen  ">
       <Navbar />
       <Hero />
       <Skills />
@@ -18,7 +20,16 @@ export default function Home() {
       <Projects />
       {/* <FigmaTool /> */}
       <Contact />
-      
+
+      <div className=' absolute bottom-0 w-full h-[80] flex justify-center items-center
+       ' >
+
+        <Link href={'/#skills'} className=' flex  justify-center items-center bg-[#12131470] w-[40] h-[40] rounded-full ' >
+          <ArrowDown />
+        </Link>
+
+      </div>
+
       <footer className="py-12 border-t border-border/50 text-center">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 space-y-4">
           <p className="text-xl font-headline font-bold tracking-tighter text-primary">
