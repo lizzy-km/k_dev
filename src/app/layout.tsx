5537a409e311c942c52bf6b4fd3f95fcd9d67ace
Fuck the Error! <kaungmyatsoe.devk@gmail.com>
2026-02-19 11:00:04 +0630

    feat: enhance Open Graph and Twitter metadata for improved SEO and social sharing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,7 @@ export const metadata: Metadata = {
     description: 'Portfolio of Kaung Myat Soe -  React Developer specializing in HRMS and Fintech solutions.',
     url: 'https://quix-dev.online',
     siteName: "Kaung Myat Soe's Portfolio",  
+    
     images: [
       {
         url: 'https://quix-dev.online/kaungMyatSoe_p.png',
@@ -51,7 +52,15 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
         <script src="https://techo-ai-chatbot-omega.vercel.app/chatbot.umd.js">
-        
+        <meta property="og:image" content="https://quix-dev.online/kaungMyatSoe_p.png"></meta>
+        <meta property="og:site_name" content="Kaung Myat Soe's Portfolio"></meta>
+        <meta property="og:title" content="Kaung Myat Soe's Portfolio | React Developer"></meta>
+        <meta property="og:description" content="Portfolio of Kaung Myat Soe -  React Developer specializing in HRMS and Fintech solutions."></meta>
+        <meta property="og:url" content="https://quix-dev.online"></meta>
+        <meta property="twitter:description" content="Portfolio of Kaung Myat Soe -  React Developer specializing in HRMS and Fintech solutions."></meta>
+        <meta property="twitter:title" content="Kaung Myat Soe's Portfolio | React Developer"></meta>
+        <meta property="twitter:card" content="summary_large_image"></meta>
+        <meta property="twitter:image" content="https://quix-dev.online/kaungMyatSoe_p.png"></meta>
         </script>
 
     
