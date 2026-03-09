384a74cf3b0f84aca6f676e501b592711f9abd60
Fuck the Error! <kaungmyatsoe.devk@gmail.com>
2026-03-09 16:40:46 +0630

    add yml

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"RqZXDNN7YyXRHNI4jcXj1sKObflcsLMFIL1uQo9FvE8=\"\n}"
---
+++ .next/static/chunks/src_app_page_tsx_6bade904._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_74351d43._.js",
+  "static/chunks/node_modules_next_eb27f87e._.js",
+  "static/chunks/node_modules_motion-dom_dist_es_bc6a3e4a._.js",
+  "static/chunks/node_modules_framer-motion_dist_es_afea34c2._.js",
+  "static/chunks/node_modules_axios_lib_99e19c7d._.js",
+  "static/chunks/node_modules_eff7dcc6._.js"
+],
+    source: "dynamic"
+});
