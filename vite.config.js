37b150671cf5ffea8314bdc1393efa255139a0b1
Fuck the Error! <kaungmyatsoe.devk@gmail.com>
2026-03-10 08:42:19 +0630

    change outDir and cacheDir

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build:{
-    outDir:'.next',
-    cacheDir:'.next/cache'
+    outDir:'dist',
   }
 })
